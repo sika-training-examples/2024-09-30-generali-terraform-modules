0dd3d858db29c7fc4ce2961f00b658b920a89f61
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 14:39:12 +0200

    feat(service_plan): Add resource group name and location to output

--- service_plan/outputs.tf
+++ service_plan/outputs.tf
@@ -1,3 +1,11 @@
 output "service_plan_id" {
   value = azurerm_service_plan.this.id
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.this.location
+}
