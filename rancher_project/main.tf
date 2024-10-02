0dc5a0fb613e510c28e814f46a76774dca8b39f0
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-02 10:37:57 +0200

    feat(rancher_project): Add quotas

--- rancher_project/main.tf
+++ rancher_project/main.tf
@@ -1,4 +1,17 @@
 resource "rancher2_project" "this" {
   name       = var.name
   cluster_id = var.cluster_id
+
+  resource_quota {
+    project_limit {
+      limits_cpu       = var.project_limit_cpu
+      limits_memory    = var.project_limit_memory
+      requests_storage = var.project_limit_storage
+    }
+    namespace_default_limit {
+      limits_cpu       = var.namespace_default_limit_cpu
+      limits_memory    = var.namespace_default_limit_memory
+      requests_storage = var.namespace_default_limit_storage
+    }
+  }
 }
--- rancher_project/variables.tf
+++ rancher_project/variables.tf
@@ -7,3 +7,39 @@ variable "cluster_id" {
   description = "The ID of the cluster to create the project in"
   type        = string
 }
+
+variable "project_limit_cpu" {
+  description = "The CPU limit for the project"
+  type        = string
+  default     = "2000m"
+}
+
+variable "project_limit_memory" {
+  description = "The memory limit for the project"
+  type        = string
+  default     = "2000Mi"
+}
+
+variable "project_limit_storage" {
+  description = "The storage limit for the project"
+  type        = string
+  default     = "2Gi"
+}
+
+variable "namespace_default_limit_cpu" {
+  description = "The default CPU limit for namespaces in the project"
+  type        = string
+  default     = "2000m"
+}
+
+variable "namespace_default_limit_memory" {
+  description = "The default memory limit for namespaces in the project"
+  type        = string
+  default     = "500Mi"
+}
+
+variable "namespace_default_limit_storage" {
+  description = "The default storage limit for namespaces in the project"
+  type        = string
+  default     = "1Gi"
+}
